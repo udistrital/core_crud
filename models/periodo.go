3fd3d5af5b160f564dd4f7881173fc136a29ffd0
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-07-10 10:19:07 -0500

    se agrega el modelo de periodo con la nueva relación de tipo periodo

--- models/periodo.go
+++ models/periodo.go
@@ -5,17 +5,21 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/astaxie/beego/orm"
 )
 
 type Periodo struct {
-	Id                int     `orm:"column(id);pk;auto"`
-	Nombre            string  `orm:"column(nombre)"`
-	Descripcion       string  `orm:"column(descripcion);null"`
-	CodigoAbreviacion string  `orm:"column(codigo_abreviacion);null"`
-	Activo            bool    `orm:"column(activo)"`
-	NumeroOrden       float64 `orm:"column(numero_orden);null"`
+	Id                int          `orm:"column(id);pk;auto"`
+	Nombre            string       `orm:"column(nombre)"`
+	Descripcion       string       `orm:"column(descripcion);null"`
+	CodigoAbreviacion string       `orm:"column(codigo_abreviacion);null"`
+	Activo            bool         `orm:"column(activo)"`
+	NumeroOrden       float64      `orm:"column(numero_orden);null"`
+	FechaCreacion     time.Time    `orm:"column(fecha_creacion);type(timestamp without time zone)"`
+	FechaModificacion time.Time    `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
+	TipoPeriodo       *TipoPeriodo `orm:"column(tipo_periodo);rel(fk)"`
 }
 
 func (t *Periodo) TableName() string {
