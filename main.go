e0f8d28d8e6432a9da7412031dde730daa21e968
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-07-18 17:11:35 -0500

    Se cambia main.go de acuertdo a los lineamento del repo titan_api_crud

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/udistrital/core_crud/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerror"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
@@ -11,16 +12,16 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func init() {
-	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
-}
-
-func main() {
-	orm.Debug = true
+func test() {
 	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+}
+
+func init() {
+	test()
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
@@ -33,12 +34,17 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
+}
 
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 	logPath := "{\"filename\":\""
 	logPath += beego.AppConfig.String("logPath")
 	logPath += "\"}"
 	logs.SetLogger(logs.AdapterFile, logPath)
-
 	apistatus.Init()
+	beego.ErrorController(&customerror.CustomErrorController{})
+
 	beego.Run()
 }
