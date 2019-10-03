babb5b842618e5b134a15722664362c58861860c
Mafe Avendaño <mafe.ud@gmail.com>
2019-10-03 07:41:22 -0500

    Add migrations

---
+++ database/scripts/20190804_124141_inserts_parametricas.down.sql
@@ -0,0 +1,10 @@
+delete from core.nucleo_basico_conocimiento;
+delete from core.area_conocimiento;
+delete from core.tipo_dato_adicional;
+delete from core.periodo;
+delete from core.tipo_periodo;
+delete from core.unidad_tiempo
+delete from core.concepto_academico;
+delete from core.linea_investigacion_grupo_investigacion;
+delete from core.grupo_investigacion;
+delete from core.linea_investigacion;
